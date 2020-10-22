05c5cfb293744ee3395cac81565a4131e90c394a
Fonsus Proguer <fonsuspro@gmail.com>
2020-10-22 11:33:06 -0500

    Agregar archivos de proyecto.

---
+++ AppTutorialesFonsus/Models/TutorialModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Text;
+
+namespace AppTutorialesFonsus.Models
+{
+    public class TutorialModel
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int TutorialId { get; set; }
+        public string TutorialTitulo { get; set; }
+        public string TutorialDescripcion { get; set; }
+    }
+}
